402f8a2067add2e3c7b2e78363d7cd2bad79b515
ColinFritzLtts <colinfritz.ai@gmail.com>
2020-06-13 22:51:31 -0500

    Molecular motion complete except for plot doesn't look as elegant, probably due to changes in matplotlib because even the solution code looks the same

--- Chapter_15_Generating_Data/try_it_yourself/Molecular_Motion.py
+++ Chapter_15_Generating_Data/try_it_yourself/Molecular_Motion.py
@@ -42,25 +42,32 @@ class RandomWalk():
 
 
 
-rw=RandomWalk()
 
+
+# Keep making new walks, as long as the program is active.
 while True:
-	rw.fill_walk() # updates the contents of the class instance data attributes 
-	point_numbers=list(range(rw.num_points))
-	plt.figure(figsize=(10,6))
-	plt.plot(rw.x_values , rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=1)
-	# emphasizing the first and last points 
-	plt.plot(0,0,c='green', edgecolors='none', s=100 ) 
-	plt.plot(rw.x_values[-1], rw.y_values[-1], c = 'red', edgecolor= 'none', s = 100) 
-
-	# Remove the axes 
-	plt.axes().get_xaxis().set_visible(False)
-	plt.axes().get_yaxis().set_visible(False)
-
-	plt.show()
-
-	keep_running = input("Make another walk? (y/n): ")
-	if keep_running == 'n':
-		break 
-
+    # Make a random walk, and plot the points.
+    rw = RandomWalk(5000)
+    rw.fill_walk()
+    
+    # Set the size of the plotting window.
+    plt.figure(dpi=128, figsize=(10, 6))
+    
+    point_numbers = list(range(rw.num_points))
+    plt.plot(rw.x_values, rw.y_values, linewidth=1)
+        
+    # Emphasize the first and last points.
+    plt.scatter(0, 0, c='green', edgecolors='none', s=75)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none',
+        s=75)
+        
+    # Remove the axes.
+    plt.axes().get_xaxis().set_visible(False)
+    plt.axes().get_yaxis().set_visible(False)
+        
+    plt.show()
+    
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break
 
